f0972c95ce2672b1cd9a20ce696380efef82e7ad
alshknv <al.shikunov81@gmail.com>
2022-05-28 15:09:16 +0300

    dont panic puzzle

---
+++ MediumPuzzles/DontPanic Ep1/Program/Program.cs
@@ -0,0 +1,127 @@
+using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Codingame
+{
+    public class Player
+    {
+        private static List<int> GetClosestElevators(int curPos, int width, List<int> elevators)
+        {
+            var result = new List<int>();
+            var closestRight = width + 1;
+            var closestLeft = -1;
+            for (int i = 0; i < elevators.Count; i++)
+            {
+                if (elevators[i] > curPos && elevators[i] < closestRight) closestRight = elevators[i];
+                if (elevators[i] < curPos && elevators[i] > closestLeft) closestLeft = elevators[i];
+            }
+            if (closestRight < width - 1) result.Add(closestRight);
+            if (closestLeft >= 0) result.Add(closestLeft);
+            return result;
+        }
+        private static (int[] path, int length) RecursiveSearch(int floor, int width, int exitFloor, int exitPos, int curPos, int genPos, List<int>[] elevators, List<int> path)
+        {
+            path = new List<int>(path)
+        {
+            curPos
+        };
+
+            if (floor == exitFloor && curPos == exitPos)
+            {
+                var prevPos = genPos;
+                var pathLength = 0;
+                for (int i = 0; i < path.Count; i++)
+                {
+                    pathLength += Math.Abs(path[i] - prevPos) + 1;
+                    prevPos = path[i];
+                }
+                return (path.ToArray(), pathLength);
+            }
+            int[] minPath = null;
+            int minLength = width * elevators.Length + 1;
+            var closestElevators = GetClosestElevators(curPos, width, elevators[floor]);
+            if (floor == exitFloor)
+                closestElevators.Add(exitPos);
+            foreach (var elevator in closestElevators)
+            {
+                var foundPath = RecursiveSearch(floor < exitFloor ? floor + 1 : floor, width, exitFloor, exitPos, elevator, genPos, elevators, path);
+                if (foundPath.length < minLength)
+                {
+                    minLength = foundPath.length;
+                    minPath = foundPath.path;
+                }
+            }
+            return (minPath, minLength);
+        }
+
+        public static (int, int)[] GetCloneBlocks(int width, int exitFloor, int exitPos, int genPos, List<int>[] elevators)
+        {
+            var (path, _) = RecursiveSearch(0, width, exitFloor, exitPos, genPos, genPos, elevators, new List<int>());
+            var blocks = new List<(int, int)>();
+            var right = true;
+            for (int i = 0; i < path.Length; i++)
+            {
+                if (i > 0 && ((right && path[i - 1] > path[i]) || (!right && path[i - 1] < path[i])))
+                {
+                    blocks.Add((i - 1, path[i - 1]));
+                    right = !right;
+                }
+            }
+            return blocks.ToArray();
+        }
+
+        static void Main(string[] args)
+        {
+            var inputs = Console.ReadLine().Split(' ');
+            int nbFloors = int.Parse(inputs[0]); // number of floors
+            int width = int.Parse(inputs[1]); // width of the area
+            int nbRounds = int.Parse(inputs[2]); // maximum number of rounds
+            int exitFloor = int.Parse(inputs[3]); // floor on which the exit is found
+            int exitPos = int.Parse(inputs[4]); // position of the exit on its floor
+            int nbTotalClones = int.Parse(inputs[5]); // number of generated clones
+            int nbAdditionalElevators = int.Parse(inputs[6]); // ignore (always zero)
+            int nbElevators = int.Parse(inputs[7]); // number of elevators
+
+            var elevators = new List<int>[nbFloors];
+            for (int i = 0; i < nbFloors; i++)
+            {
+                elevators[i] = new List<int>();
+            }
+            for (int i = 0; i < nbElevators; i++)
+            {
+                inputs = Console.ReadLine().Split(' ');
+                int elevatorFloor = int.Parse(inputs[0]); // floor on which this elevator is found
+                int elevatorPos = int.Parse(inputs[1]); // position of the elevator on its floor
+                elevators[elevatorFloor].Add(elevatorPos);
+            }
+
+            (int floor, int pos)[] blocks = null;
+            var currentBlock = 0;
+            // game loop
+            while (true)
+            {
+                inputs = Console.ReadLine().Split(' ');
+                int cloneFloor = int.Parse(inputs[0]); // floor of the leading clone
+                int clonePos = int.Parse(inputs[1]); // position of the leading clone on its floor
+                string direction = inputs[2]; // direction of the leading clone: LEFT or RIGHT
+                if (blocks == null)
+                {
+                    blocks = GetCloneBlocks(width, exitFloor, exitPos, clonePos, elevators);
+                }
+                if (currentBlock < blocks.Length && cloneFloor == blocks[currentBlock].floor && clonePos == blocks[currentBlock].pos)
+                {
+                    Console.WriteLine("BLOCK");
+                    currentBlock++;
+                }
+                else
+                {
+                    Console.WriteLine("WAIT"); // action: WAIT or BLOCK
+                }
+            }
+        }
+    }
+}
---
+++ MediumPuzzles/DontPanic Ep1/Tests/ProgramTests.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+using Codingame;
+using FluentAssertions;
+
+namespace Tests
+{
+    public class PlayerTests
+    {
+        [Fact]
+        public void SimpleTest()
+        {
+            var blocks = Player.GetCloneBlocks(12, 3, 11, 3, new List<int>[] {
+                new List<int>() { 1, 9 },
+                new List<int>() { 2, 5, 11 },
+                new List<int>() { 4, 10 },
+                new List<int>(),
+            });
+            blocks.Should().HaveCount(1);
+            blocks[0].Should().Be((0, 3));
+        }
+    }
+}
